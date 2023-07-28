d24ffb76a3187ea4935269f8bdb0f2dba8c76631
Sumanani <bharani.bbk@gmail.com>
2023-07-29 04:25:50 +0530

    fixed import.

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -10,7 +10,7 @@ import {
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { MdPhoneIphone } from "react-icons/md";
-import { Platform } from "./../hooks/useGames";
+import { Platform } from "./../hooks/usePlatform";
 import { IconType } from "react-icons";
 
 interface Props {
