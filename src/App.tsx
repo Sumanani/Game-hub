6c4bed7c10907bce3f230b270ee08301e076bd7b
Sumanani <bharani.bbk@gmail.com>
2023-07-28 21:28:01 +0530

    filter games by platform.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenresList from "./components/GenresList";
-import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Genre } from "./hooks/useGenres";
+import { Platform } from "./hooks/usePlatform";
 
 const App = () => {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   const onSelectGenre = (genre: Genre) => {
     setSelectedGenre(genre);
+  };
+
+  const onSelectPlatform = (platform: Platform) => {
+    setSelectedPlatform(platform);
   };
 
   return (
@@ -21,7 +29,7 @@ const App = () => {
       }}
       templateColumns={{
         base: "1fr",
-        lg: "200px 1fr",
+        lg: "225px 1fr",
       }}
     >
       <GridItem area="nav">
@@ -36,8 +44,14 @@ const App = () => {
         </GridItem>
       </Show>
       <GridItem padding="16px" area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector
+          selectedPlatform={selectedPlatform}
+          onSelectPlatform={onSelectPlatform}
+        />
+        <GameGrid
+          selectedPlatform={selectedPlatform}
+          selectedGenre={selectedGenre}
+        />
       </GridItem>
     </Grid>
   );
